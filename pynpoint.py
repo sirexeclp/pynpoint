0ace2de113d98568a455ecd2c79cac3438469f25
felix <sirexeclp@gmail.com>
2020-10-01 21:54:21 +0200

    init

---
+++ pynpoint.py
@@ -0,0 +1,31 @@
+import ctypes
+from typing import Optional, Tuple
+
+class MCP:
+    def __init__(self, port:str):
+        if not isinstance(port, str):
+            raise TypeError("port must be of type str!")
+        self.port:str = port
+        self.lib = ctypes.CDLL("libmcp.so")
+        self.handle: Optional[int] = None
+
+    def _init(self) -> None:
+        port = ctypes.c_char_p(self.port)
+        handle = self.lib.f511_init(port)
+        if handle < 0:
+            raise IOError(f"Got non negative return value from init function. ({handle})")
+        self.handle = handle
+
+
+    def get_power(self) -> Tuple[int, int]:
+        if self.handle is None:
+            raise IOError("Handle is not initialized!")
+        
+        channel1 = ctypes.c_int
+        channel2 = ctypes.c_int
+        result = self.lib.f511_get_power(ctypes.byref(channel1),
+                 ctypes.byref(channel2), ctypes.c_int(self.handle))
+        if result.value == 0:
+            return channel1.value, channel2.value
+        else:
+            raise IOError(f"Got non zero return code ({result.value})!")
