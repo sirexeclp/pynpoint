f5fdd9c55a8a4de3a6d78332effe18257b88670e
felix <sirexeclp@gmail.com>
2020-10-28 14:28:51 +0100

    fix ioerror and refactor

--- pynpoint/pynpoint.py
+++ pynpoint/pynpoint.py
@@ -2,33 +2,28 @@ import ctypes
 from typing import Optional, Tuple
 import pathlib
 
+
 class MCP:
-    def __init__(self, port:str):
+    def __init__(self, port: str):
         if not isinstance(port, str):
             raise TypeError("port must be of type str!")
-        self.port:str = port
+
+        self.port = port
         lib_path = pathlib.Path(__file__).parent.absolute() / "libmcp.so"
-        self.lib = ctypes.CDLL(lib_path)
-        self.handle: Optional[int] = None
-        self._init()
+        self._lib = ctypes.CDLL(lib_path)
 
-    def _init(self) -> None:
-        port = ctypes.c_char_p(self.port.encode())
-        handle = self.lib.f511_init(port)
-        if handle < 0:
-            raise IOError(f"Got non negative return value from init function. ({handle})")
-        self.handle = handle
-
+        c_port = ctypes.c_char_p(self.port.encode())
+        self.handle = self._lib.f511_init(c_port)
+        if self.handle < 0:
+            raise IOError(f"Got non negative return value from init function. ({self.handle})")
 
     def get_power(self) -> Tuple[int, int]:
-        if self.handle is None:
-            raise IOError("Handle is not initialized!")
-        
         channel1 = ctypes.c_int()
         channel2 = ctypes.c_int()
-        result: int = self.lib.f511_get_power(ctypes.byref(channel1),
-                 ctypes.byref(channel2), ctypes.c_int(self.handle))
+        result: int = self._lib.f511_get_power(ctypes.byref(channel1),
+                                               ctypes.byref(channel2),
+                                               ctypes.c_int(self.handle))
         if result == 0:
             return channel1.value, channel2.value
         else:
-            raise IOError(f"Got non zero return code ({result.value})!")
+            raise IOError(f"Got non zero return code ({result})!")
